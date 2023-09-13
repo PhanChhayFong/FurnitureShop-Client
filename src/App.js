2dd5ada8f258e8166fefaa3cca95094b5f8a1b59
ReactGroupWeb <react.project.2023@gmail.com>
2023-09-13 18:03:07 +0700

    Frontend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 // import './App.css';
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, lazy, Suspense } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
   BrowserRouter as Router,
@@ -12,31 +12,57 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
 // component
-import MenuNavbar from "./components/MenuNavbar";
-import Footer from "./components/footer";
-import ApiService from "./services/api-service";
+// import MenuNavbar from "./components/MenuNavbar";
+// import Footer from "./components/footer";
+// import ApiService from "./services/api-service";
+// // page
+// import HomePage from "./screen/HomePage";
+// import AboutUs from "./screen/AboutUs";
+// import Contact from "./screen/Contact";
+// import Shop from "./screen/Shop";
+
+// import ProductDetail from "./screen/ProductDetail";
+// import ProductCategory from "./screen/ProductCategory";
+// import Cart from "./screen/Cart";
+// import Wishlist from "./screen/Wishlist";
+// import Checkout from "./screen/Checkout";
+
+// import Login from "./screen/Login";
+// import SignUp from "./screen/SignUp";
+// import ProtectedRoute from "./components/ProtectedRoute";
+
+// import MyDashboard from "./screen/MyDashboard";
+// import OrderDetail from "./screen/OrderDetail";
+// import MyAccount from "./screen/MyAccount";
+
+// // page 404 not found
+// import Page404 from "./screen/page404";
+
+const MenuNavbar = lazy(() => import("./components/MenuNavbar"));
+const Footer = lazy(() => import("./components/footer"));
+const ApiService = lazy(() => import("./services/api-service"));
 // page
-import HomePage from "./screen/HomePage";
-import AboutUs from "./screen/AboutUs";
-import Contact from "./screen/Contact";
-import Shop from "./screen/Shop";
+const HomePage = lazy(() => import("./screen/HomePage"));
+const AboutUs = lazy(() => import("./screen/AboutUs"));
+const Contact = lazy(() => import("./screen/Contact"));
+const Shop = lazy(() => import("./screen/Shop"));
 
-import ProductDetail from "./screen/ProductDetail";
-import ProductCategory from "./screen/ProductCategory";
-import Cart from "./screen/Cart";
-import Wishlist from "./screen/Wishlist";
-import Checkout from "./screen/Checkout";
+const ProductDetail = lazy(() => import("./screen/ProductDetail"));
+const ProductCategory = lazy(() => import("./screen/ProductCategory"));
+const Cart = lazy(() => import("./screen/Cart"));
+const Wishlist = lazy(() => import("./screen/Wishlist"));
+const Checkout = lazy(() => import("./screen/Checkout"));
 
-import Login from "./screen/Login";
-import SignUp from "./screen/SignUp";
-import ProtectedRoute from "./components/ProtectedRoute";
+const Login = lazy(() => import("./screen/Login"));
+const SignUp = lazy(() => import("./screen/SignUp"));
+const ProtectedRoute = lazy(() => import("./components/ProtectedRoute"));
 
-import MyDashboard from "./screen/MyDashboard";
-import OrderDetail from "./screen/OrderDetail";
-import MyAccount from "./screen/MyAccount";
+const MyDashboard = lazy(() => import("./screen/MyDashboard"));
+const OrderDetail = lazy(() => import("./screen/OrderDetail"));
+const MyAccount = lazy(() => import("./screen/MyAccount"));
 
 // page 404 not found
-import Page404 from "./screen/page404";
+const Page404 = lazy(() => import("./screen/page404"));
 
 export default function App() {
   const [stripeAPIKey, setStripeAPIKey] = useState("");
@@ -64,74 +90,74 @@ export default function App() {
   return (
     <Router>
       <MenuNavbar />
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/shop/product_detail/:id" element={<ProductDetail />} />
-        <Route
-          path="/shop/product_category/:id"
-          element={<ProductCategory />}
-        />
-        <Route
-          path="/shop/cart"
-          element={
-            <ProtectedRoute>
-              <Cart />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/shop/wishlist"
-          element={
-            <ProtectedRoute>
-              <Wishlist />
-            </ProtectedRoute>
-          }
-        />
-        {stripeAPIKey && (
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/shop/product_detail/:id" element={<ProductDetail />} />
           <Route
-            path="/checkout"
+            path="/shop/product_category/:id"
+            element={<ProductCategory />}
+          />
+          <Route
+            path="/shop/cart"
             element={
-              <Elements stripe={loadStripe(stripeAPIKey)}>
-                <ProtectedRoute>
-                  <Checkout />
-                </ProtectedRoute>
-              </Elements>
+              <ProtectedRoute>
+                <Cart />
+              </ProtectedRoute>
             }
           />
-        )}
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/sign-up" element={<SignUp />}></Route>
-        <Route
-          path="/my-dashboard"
-          element={
-            <ProtectedRoute>
-              <MyDashboard />
-            </ProtectedRoute>
-          }
-        ></Route>
-        <Route
-          path="/my-dashboard/order-detail/:id"
-          element={
-            <ProtectedRoute>
-              <OrderDetail />
-            </ProtectedRoute>
-          }
-        ></Route>
-        <Route
-          path="/my-account"
-          element={
-            <ProtectedRoute>
-              <MyAccount />
-            </ProtectedRoute>
-          }
-        ></Route>
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-
+          <Route
+            path="/shop/wishlist"
+            element={
+              <ProtectedRoute>
+                <Wishlist />
+              </ProtectedRoute>
+            }
+          />
+          {stripeAPIKey && (
+            <Route
+              path="/checkout"
+              element={
+                <Elements stripe={loadStripe(stripeAPIKey)}>
+                  <ProtectedRoute>
+                    <Checkout />
+                  </ProtectedRoute>
+                </Elements>
+              }
+            />
+          )}
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/sign-up" element={<SignUp />}></Route>
+          <Route
+            path="/my-dashboard"
+            element={
+              <ProtectedRoute>
+                <MyDashboard />
+              </ProtectedRoute>
+            }
+          ></Route>
+          <Route
+            path="/my-dashboard/order-detail/:id"
+            element={
+              <ProtectedRoute>
+                <OrderDetail />
+              </ProtectedRoute>
+            }
+          ></Route>
+          <Route
+            path="/my-account"
+            element={
+              <ProtectedRoute>
+                <MyAccount />
+              </ProtectedRoute>
+            }
+          ></Route>
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </Router>
   );
